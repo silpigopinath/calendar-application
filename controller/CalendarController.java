445e6ee52da914e56bbf764ae9291c033dd47aca
ashnajasmin28 <55691232+ashnajasmin28@users.noreply.github.com>
2019-10-30 12:45:06 +0530

    Add files via upload

---
+++ controller/CalendarController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.util.Date;
+import java.util.List;
+
+import dao.EventDAO;
+import model.Event;
+
+public class CalendarController {
+	private Event evnt;
+	
+	CalendarController(Event evnt)
+	{
+		this.evnt=evnt;
+	}
+	public void addEvent()
+	{
+		EventDAO.insert(evnt);
+		
+	}
+	public static List<Event> readMonth(Date d)
+	{
+		List<Event> ls=EventDAO.readByMonth(d);
+		return ls;
+		
+	}
+	public static List<Event> readDay(Date d) throws Exception
+	{
+		List<Event> ls=EventDAO.readByDay(d);
+		return ls;
+	}
+	public static List<Event> readWeek(Date d) throws Exception
+	{
+		List<Event> ls=EventDAO.readByWeek(d);
+		return ls;
+	}
+
+}
