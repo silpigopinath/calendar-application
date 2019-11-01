e85602254677697f0b7a1a0a14d8ee81a77b9cff
ashnajasmin28 <55691232+ashnajasmin28@users.noreply.github.com>
2019-11-01 14:27:02 +0530

    Add files via upload

--- model/Event.java
+++ model/Event.java
@@ -2,74 +2,147 @@ package model;
 
 
 
+
+
+
+
 import java.util.Date;
 
+
+
 public class Event {
+
 	
-	String eventId,location,title,description;
+
+	String location,title,description;
+
     Date startDate,startTime, endDate, endTime;
+
 	
-	public Event(String eventId, String location, String title, String description, Date  startDate, Date startTime, Date endDate, Date endTime) {
+
+	public Event(String title, String location,  String description, Date  startDate, Date startTime, Date endDate, Date endTime) {
+
 		super();
-		this.eventId = eventId;
+
+
 		this.location = location;
+
 		this.title = title;
+
 		this.description = description;
+
 		this.startDate = startDate;
+
 		this.startTime = startTime;
+
 		this.endDate = endDate;
+
 		this.endTime = endTime;
+
 	}
+
 	
+
 	public Date getStartDate() {
+
 		return startDate;
+
 	}
+
 	public void setStartDate(Date startDate) {
+
 		this.startDate = startDate;
+
 	}
+
 	public Date getStartTime() {
+
 		return startTime;
+
 	}
+
 	public void setStartTime(Date startTime) {
+
 		this.startTime = startTime;
+
 	}
+
 	public Date getEndDate() {
+
 		return endDate;
+
 	}
+
 	public void setEndDate(Date endDate) {
+
 		this.endDate = endDate;
+
 	}
+
 	public Date getEndTime() {
+
 		return endTime;
+
 	}
+
 	public void setEndTime(Date endTime) {
+
 		this.endTime = endTime;
+
 	}
-	public String getEventId() {
-		return eventId;
-	}
-	public void setEventId(String eventId) {
-		this.eventId = eventId;
-	}
+
+//	public String getEventId() {
+//
+//		return eventId;
+//
+//	}
+//
+//	public void setEventId(String eventId) {
+//
+//		this.eventId = eventId;
+//
+//	}
+
 	public String getLocation() {
+
 		return location;
+
 	}
+
 	public void setLocation(String location) {
+
 		this.location = location;
+
 	}
+
 	public String getTitle() {
+
 		return title;
+
 	}
+
 	public void setTitle(String title) {
+
 		this.title = title;
+
 	}
+
 	public String getDescription() {
+
 		return description;
+
 	}
+
 	public void setDescription(String description) {
+
 		this.description = description;
+
 	}
+
 	
+
 	
+
+
 
 }
